73951c76325d1b21fbbd729ce735fda3ab19a70f
Hatice Kamalak <htckamalakk@gmail.com>
2022-08-18 14:34:42 +0300

    bbbb

--- src/Gittttt.java
+++ src/Gittttt.java
@@ -2,6 +2,7 @@ public class Gittttt {
     public static void main(String[] args) {
 
         System.out.println("bu calisma sonuncu calisma");
+        System.out.println("bu calisma sonuncu calisma 222");
 
     }
 }
